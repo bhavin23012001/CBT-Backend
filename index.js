def07b9c21c980a767ad3753e119ba44739f8d58
Makwana Bhavin <90304191+Bhavin2301@users.noreply.github.com>
2025-02-20 12:48:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB database");
 }).catch((error) => {
   console.error("MongoDB connection error occured:", error);
 });
